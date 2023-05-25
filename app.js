a2289b5d754af11ce06f1755690a6f155db1b487
sherryylin0719 <sherryylin0719@gmail.com>
2023-05-25 22:54:59 +0900

    feat: add mongoose

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const bcrypt = require('bcryptjs')
 const routes = require("./routes")
 const app = express()
 const PORT = 3000
+require("./config/mongoose")
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -14,6 +19,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
-app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Express is listening on localhost:${process.env.PORT}`)
 })
---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+//use dotenv only when under non-production environment
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+
+//connect to mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
+//connection status
+const db = mongoose.connection
+db.on("error", () => {
+  console.log("mongodb error!")
+})
+db.once("open", () => {
+  console.log("mongodb connected!")
+})
+
+module.exports = db
