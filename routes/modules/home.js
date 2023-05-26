247d5f46dd687b219b8273b457f03c6273385580
sherryylin0719 <sherryylin0719@gmail.com>
2023-05-27 00:06:30 +0900

    feat: modify search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -24,4 +24,30 @@ router.get('/', (req, res) => {
   })
 })
 
+router.post('/', (req, res) => {
+  const userId = req.user._id
+  const { categoryId } = req.body
+  if (categoryId === "all") {
+    return res.redirect('/')
+  }
+  return CategoryModel.find()
+  .lean()
+  .then((categories) => {
+    return RecordModel.find({ userId, categoryId})
+      .populate('categoryId')
+      .lean()
+      .sort({ date: 'desc' })
+      .then((records) => {
+        let totalAmount = 0
+        records.forEach((record) => {
+          record.date = record.date.toISOString().slice(0,10)
+          totalAmount += record.amount
+        })
+        return res.render('index', { records, categories, totalAmount })
+      })
+  .catch(error => console.log(error))
+  })
+})
+
+
 module.exports = router
