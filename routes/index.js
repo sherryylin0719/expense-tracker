4b030f45ddbfcd71399f68368cf0c7db7e75c955
sherryylin0719 <sherryylin0719@gmail.com>
2023-05-26 23:32:55 +0900

    feat: add route (auth)

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const home = require("./modules/home")
 const users = require('./modules/users')
 const records = require('./modules/records')
+const auth = require('./modules/auth') 
 const { authenticator } = require('../middleware/auth') 
 
 router.use("/users", users)
 router.use("/records",authenticator, records)
+router.use('/auth', auth)
 router.use("/", authenticator, home)
 
 module.exports = router
