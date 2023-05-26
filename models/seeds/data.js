4bb88a4e9af71192010237e31d17689ce1775819
sherryylin0719 <sherryylin0719@gmail.com>
2023-05-26 23:23:40 +0900

    feat: modify hbs(edit, index) & record route

--- models/seeds/data.js
+++ models/seeds/data.js
@@ -37,31 +37,31 @@ const users = [
 const records = [
   {
     name: "熱狗",
-    date: 2023-05-01,
+    date: "2023-05-01",
     amount: 45,
     category: "餐飲食品"
   },
   {
     name: "公車",
-    date: 2023-05-03,
+    date: "2023-05-03",
     amount: 20,
     category: "交通出行"
   },
   {
     name: "膠帶",
-    date: 2023-05-06,
+    date: "2023-05-06",
     amount: 30,
     category: "其他"
   },
   {
     name: "椅子",
-    date: 2023-05-10,
+    date: "2023-05-10",
     amount: 300,
     category: "家居物業"
   },
   {
     name: "電影",
-    date: 2023-05-16,
+    date: "2023-05-16",
     amount: 230,
     category: "休閒娛樂"
   },
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,18 +1,57 @@
 const express = require("express")
 const router = express.Router()
 const RecordModel = require('../../models/record')
+const CategoryModel = require('../../models/category')
 const { categories } = require('../../models/seeds/data')
 
-router.get("/new", (req, res) => {
-  return res.render("new")
+router.get('/new', (req, res) => {
+  return CategoryModel.find()
+    .lean()
+    .then((categories) => res.render("new", { categories }))
+    .catch(error => console.log(error))
+});
+
+router.post("/new", (req, res) => {
+  const userId = req.user._id
+  const recordBody = req.body
+  return RecordModel.create({ ...recordBody, userId })
+    .then(() => res.redirect("/"))
+    .catch(error => console.log(error))
 })
 
-router.get('/:id/edit', (req, res) => {
+router.get("/:id/edit", (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id;
+  CategoryModel.find()
+    .lean()
+    .then((categories) => {
+      return RecordModel.findOne({ _id, userId })
+        .populate('categoryId')
+        .lean()
+        .then((record) => {
+          record.date = record.date.toISOString().slice(0, 10)
+          res.render('edit', { record, categories })
+        })
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
+});
+
+router.put('/:id/edit', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  const recordBody = req.body
+  return RecordModel.findOneAndUpdate({ _id, userId }, { ...recordBody })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.delete("/:id", (req, res) => {
   const userId = req.user._id
   const _id = req.params.id
   return RecordModel.findOne({_id, userId})
-    .lean()
-    .then(record => res.render("edit", { record, categories }))
+    .then(record => record.remove())
+    .then(() => res.redirect("/"))
     .catch(error => console.log(error))
 })
 
